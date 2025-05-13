634bbe0444bb02ef1bd637f609213ebfcb6d5d09
Jose <159615295+JoseOmolon@users.noreply.github.com>
2025-05-13 22:09:46 +0200

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,7 +1,13 @@
 int ledPin = 13; // Most boards have a built-in LED here
 
 void setup() {
- 
- pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
 }
 
